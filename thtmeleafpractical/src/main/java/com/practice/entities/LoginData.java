442fa1228d265e83d0c44564c4f84111efc3e766
DeepakKumarSingh27 <69988805+DeepakKumarSingh27@users.noreply.github.com>
2023-10-05 13:10:05 +0530

    first

---
+++ thtmeleafpractical/src/main/java/com/practice/entities/LoginData.java
@@ -0,0 +1,22 @@
+package com.practice.entities;
+
+public class LoginData {
+	private String userName;
+	private String emal;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmal() {
+		return emal;
+	}
+
+	public void setEmal(String emal) {
+		this.emal = emal;
+	}
+}
